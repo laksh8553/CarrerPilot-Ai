818a8e8603669db14663d54174195c936e7b7ff0
Laksh8553 <Lakshay22122004@gmail.com>
2026-05-01 19:58:22 +0530

    added port

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
+const PORT =process.env.PORT || 3000;
+
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true 
