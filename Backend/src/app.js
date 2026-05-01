b140f436b5c7703da7580d52ac4664b29d1759c9
Laksh8553 <Lakshay22122004@gmail.com>
2026-05-01 20:22:53 +0530

    connected backend with frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,12 @@ const cors = require('cors');
 const app = express();
 const PORT =process.env.PORT || 3000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true 
-}));
+app.use(
+  cors({
+    origin: "https://carrerpilot-ai-tool.onrender.com",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
